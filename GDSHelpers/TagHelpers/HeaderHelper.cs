b6d5e294a1be2805c40de199f26171eac8a5f92e
Paul Senior <paul.senior@cqc.org.uk>
2019-03-01 15:17:49 +0000

    Initial Project with helpers

---
+++ GDSHelpers/TagHelpers/HeaderHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace GDSHelpers.TagHelpers
+{
+
+    [HtmlTargetElement("gds-header")]
+    public class HeaderHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "header";
+            output.Attributes.SetAttribute("class", "govuk-header");
+            output.Attributes.SetAttribute("role", "banner");
+            output.Attributes.SetAttribute("data-module", "header");
+
+            var children = await output.GetChildContentAsync();
+            output.Content.SetHtmlContent(children);
+        }
+    }
+
+}
