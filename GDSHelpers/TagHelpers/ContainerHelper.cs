7d4a0da664620b562d9880ccf78b9b277362d50d
Gopikrishna Govindarajan <GovindarajanG@cqc.org.uk>
2019-05-02 10:48:11 +0100

    Update Div tag for step by step

--- GDSHelpers/TagHelpers/ContainerHelper.cs
+++ GDSHelpers/TagHelpers/ContainerHelper.cs
@@ -6,10 +6,12 @@ namespace GDSHelpers.TagHelpers
     [HtmlTargetElement("gds-container")]
     public class ContainerHelper : TagHelper
     {
+        [HtmlAttributeName("class")]
+        public string CustomClass { get; set; }
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "div";
-            output.Attributes.SetAttribute("class", "govuk-width-container");
+            output.Attributes.SetAttribute("class", $"govuk-width-container {(string.IsNullOrEmpty(CustomClass) ? "" : CustomClass)}");
             output.TagMode = TagMode.StartTagAndEndTag;
         }
     }
