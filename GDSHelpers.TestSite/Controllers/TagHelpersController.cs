b61532a9e4398f67ff21380fa5e85da9b6f6c459
Richard Garside <richard.garside@cqc.org.uk>
2019-12-20 11:40:26 +0000

    Merged PR 1053: Sample Test Site
    I've added a test website project to the solution that will allow you to test your GDS helpers in the project.
    
    It's a work in progress and needs improving, but thought others working on GDS helpers would find it helpful.

---
+++ GDSHelpers.TestSite/Controllers/TagHelpersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GDSHelpers.TestSite.Controllers
+{
+    [Route("tags")]
+    public class TagHelpersController : Controller
+    {
+        [Route("{tagView:regex([[a-zA-Z0-9]]*)}")]
+        public IActionResult Tag(string tagView)
+        {
+            return View(tagView);
+        }
+    }
+}
---
+++ GDSHelpers.TestSite/Models/SampleModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GDSHelpers.TestSite.Models
+{
+    public class SampleModel
+    {
+        public string Name { get; set; }
+
+        [Display(Name = "Interesting fact")]
+        public string InterestingFact { get; set; }
+    }
+}
